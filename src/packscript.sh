e17fe47c7014a9c4fc4bc59799437ca2bcfc532a
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2020-05-02 16:31:33 +0200

    Detach after launch

--- src/packscript.sh
+++ src/packscript.sh
@@ -2,13 +2,12 @@
 
 SRC="srv/MiSTer_WebMenu"
 HASH=$(sha256sum -b < "$SRC")
-# BIN="/media/fat/WebMenu"
-BIN="/tmp/WebMenu"
+BIN="/media/fat/WebMenu"
 
 cat <<-EOF
 	#!/usr/bin/env bash
-	[ -x "$BIN" ] && sha256sum -c <(echo "$HASH") < "$BIN" > /dev/null 2>&1 && exec "$BIN"
-	uudecode -o - "\$0" | xzcat -d -c > "$BIN" && chmod a+x "$BIN" && exec "$BIN"
+	[ -x "$BIN" ] && sha256sum -c <(echo "$HASH") < "$BIN" > /dev/null 2>&1 && ("$BIN" &) && exit 0
+	uudecode -o - "\$0" | xzcat -d -c > "$BIN" && chmod a+x "$BIN" && ("$BIN" &) && exit 0
 	echo "Something went wrong, run with 'bash -x' and report the error" >&2 && exit 1
 EOF
 uuencode - < <(xzcat -z < "$SRC")
