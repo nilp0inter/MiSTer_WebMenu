afa027499c1e927d2fe411561a9423dc948d9d7d
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2020-05-02 18:27:34 +0200

    Umount WebMenu MiSTer config overlay if mounted

--- src/srv/main.go
+++ src/srv/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"os/exec"
+	"syscall"
 
 	"github.com/rakyll/statik/fs"
 
@@ -11,8 +13,35 @@ import (
 	_ "github.com/nilp0inter/MiSTer_WebMenu/statik"
 )
 
+func umountConfig() {
+	for {
+		cmd := exec.Command("/bin/umount", "/media/fat/MiSTer.ini")
+
+		if err := cmd.Start(); err != nil {
+			log.Fatalf("Cannot umount MiSTer config: %v", err)
+		}
+
+		if err := cmd.Wait(); err != nil {
+			if exiterr, ok := err.(*exec.ExitError); ok {
+				if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+					if st := status.ExitStatus(); st == 32 {
+						// Nothing is mounted
+						return
+					} else {
+						log.Fatalf("Error unmounting MiSTer config: ", st)
+					}
+				}
+			} else {
+				log.Fatalf("cmd.Wait: %v", err)
+				continue
+			}
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Starting MiSTer WebMenu!")
+	umountConfig()
 
 	statikFS, err := fs.New()
 	if err != nil {
