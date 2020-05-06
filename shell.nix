b82173f472c7ff593f7c0e71d1e6682f0378837f
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2020-05-06 23:58:24 +0200

    simplify nix setup and pinned version

---
+++ shell.nix
@@ -0,0 +1,17 @@
+with import (builtins.fetchTarball {
+  name = "nixos-unstable-2018-09-12";
+  url = "https://github.com/nixos/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz";
+  sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+}) {}; {
+  webmenuEnv = stdenv.mkDerivation {
+    name = "WebMenu-Env";
+    buildInputs = [ sharutils
+                    go
+                    git
+                    statik
+                    elmPackages.elm
+                    nodePackages.uglify-js
+                    elmPackages.elm-live
+                  ];
+  };
+}
--- src/web/shell.nix
+++
@@ -1,9 +0,0 @@
-{
-  pkgs ? import <nixpkgs> {}
-}:
-
-with pkgs;
-stdenv.mkDerivation {
-  name = "WebMenu";
-  buildInputs = [ elmPackages.elm elmPackages.elm-live ];
-}
