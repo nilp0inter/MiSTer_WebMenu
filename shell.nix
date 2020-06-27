1a1d1ee06beb36ab758d55a7198d63c228cf65c9
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2020-06-27 12:14:46 +0200

    chore: Use mkShell to setup the environment

--- shell.nix
+++ shell.nix
@@ -1,20 +1,21 @@
-with import (builtins.fetchTarball {
-  name = "nixos-unstable-2018-09-12";
-  url = "https://github.com/nixos/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz";
-  sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
-}) {}; {
-  webmenuEnv = stdenv.mkDerivation {
-    name = "WebMenu-Env";
-    buildInputs = [ sharutils
-                    go
-                    git
-                    statik
-                    elmPackages.elm
-		    entr
-                    nodePackages.uglify-js
-                    nodePackages.elm-oracle
-                    elmPackages.elm-format
-                    elmPackages.elm-live
-                  ];
-  };
+{ pkgs ? import (builtins.fetchTarball {
+    name = "nixos-unstable-2018-09-12";
+    url = "https://github.com/nixos/nixpkgs/archive/5272327b81ed355bbed5659b8d303cf2979b6953.tar.gz";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+  }) {}}: 
+pkgs.mkShell {
+  name = "dev-webmenu";
+  buildInputs = [
+    pkgs.sharutils
+    pkgs.go
+    pkgs.git
+    pkgs.statik
+    pkgs.elmPackages.elm
+    pkgs.entr
+    pkgs.nodePackages.uglify-js
+    pkgs.nodePackages.elm-oracle
+    pkgs.elmPackages.elm-format
+    pkgs.elmPackages.elm-live
+  ];
 }
+
